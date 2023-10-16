d69f8f9a786ce208592ca48506eda12f18fd162e
Daniel Plundrich <dan.plundrich@gmail.com>
2023-10-16 08:40:54 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function dropDownSearchBar() {
+    document.querySelector(".dropdown-search-bar-content").classList.toggle("show");
+  }
+
+  function dropDownMenu() {
+    document.querySelector(".grid-menu-list-content").classList.toggle("show");
+    document.querySelector(".category-link").classList.toggle("category-link-active");
+
+
+  }
+
+
