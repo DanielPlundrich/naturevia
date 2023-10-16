4fa79f7ccf41357369e28056bd7c9a51b330f955
Daniel Plundrich <dan.plundrich@gmail.com>
2023-10-16 22:56:46 +0200

    dokončeno

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ function dropDownSearchBar() {
   function dropDownMenu() {
     document.querySelector(".grid-menu-list-content").classList.toggle("show");
     document.querySelector(".category-link").classList.toggle("category-link-active");
-
-
   }
 
-
+  function dropDownMenuMobile() {
+    document.querySelector(".mobile-menu-dropdown").classList.toggle("show");
+    document.querySelector(".menu-mobile-item").classList.toggle("menu-mobile-item-active");
+  }
